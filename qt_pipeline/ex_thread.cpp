91c9124ef74318bb13e4ec597a72be5ae3c148e9
Marco Mora <54404955+MarcoMora85@users.noreply.github.com>
2019-11-01 09:47:21 -0600

    Implementacion de la etapa EX.
    Se implementaron el funcionamiento de la etapa EX con subrutinas que representan las acciones a tomar con cada instruccion.(addi,add,sub,...), incluso el funcionamiento de los branches(sujeto a pruebas).
    Se implemento tambien la funcion executePhase() la cual evalua en un switch la instruccion en IR y a partir de eso ejecuta la subrutina respectiva al codigo de operacion, en esta funcion tambien se hace el manejo de los NOPs en caso de branches verdaderos.

--- qt_pipeline/ex_thread.cpp
+++ qt_pipeline/ex_thread.cpp
@@ -8,5 +8,149 @@ ex_thread::ex_thread()
 void ex_thread::run(void *data){
     //Espera a que el master thread inicialice todo
     final_bar->Wait();
-    cout<<"Hola from ex"<<endl;
+    //cout<<"Hola from ex"<<endl;
+    executePhase();
+}
+
+void ex_thread::executePhase(){
+    //Condicion con Estado MEM
+    if (branch_result && overwrite_cycles < 2){
+        overwrite_cycles++;
+        passInstrToMem(0); //Pasar 2 NOPs a MEM
+    }else{
+        branch_result = false;
+        overwrite_cycles = 0;
+        switch (input_box[0]) {
+            case 19:
+                addi();
+                break;
+            case 71:
+                add();
+                break;
+            case 83:
+                sub();
+                break;
+            case 72:
+                mul();
+                break;
+            case 56:
+                div();
+                break;
+            case 5:
+                lw();
+                break;
+            case 37:
+                sw();
+                break;
+            case 99:
+                beq();
+                break;
+            case 100:
+                bne();
+                break;
+            case 51:
+                lr();
+                break;
+            case 52:
+                sc();
+                break;
+            case 111:
+                jal();
+                break;
+            case 103:
+                jalr();
+                break;
+            default:
+                //FIN o NOP
+                cout << "No operation executed in EX" << endl;
+        }
+
+        passInstrToMem(input_box[0]);
+    }
+}
+
+void ex_thread::passInstrToMem(int instruction){
+    output_box[1] = instruction;
+}
+
+void ex_thread::addi(){
+    //Suma Imm y registro A y lo pasa a ALU Out
+    output_box[2] = input_box[1] + input_box[3];
+}
+
+void ex_thread::add(){
+    //Suma registro A y B y lo pasa a ALU Out
+    output_box[2] = input_box[1] + input_box[2];
+}
+
+void ex_thread::sub(){
+    //Resta registro A y B y lo pasa a ALU Out
+    output_box[2] = input_box[1] - input_box[2];
+}
+
+void ex_thread::mul(){
+    //Multiplica registro A y B y lo pasa a ALU Out
+    output_box[2] = input_box[1] * input_box[2];
+}
+
+void ex_thread::div(){
+    //Divide registro A y B y lo pasa a ALU Out
+    output_box[2] = input_box[1] / input_box[2];
+}
+
+void ex_thread::lw(){
+    //Calcula la direccion sumando el registro A e Imm
+    output_box[2] = input_box[1] + input_box[3];
+}
+
+void ex_thread::lr(){
+    //Calcula la direccion sumando el registro A e Imm
+    output_box[2] = input_box[1];
+}
+
+void ex_thread::sw(){
+    //Calcula la direccion sumando el registro A e Imm y pasa el registro B a MEM
+    output_box[2] = input_box[1] + input_box[3];
+    output_box[3] = input_box[2];
+}
+
+void ex_thread::sc(){
+    //Calcula la direccion sumando el registro A e Imm y pasa el registro B a MEM
+    output_box[2] = input_box[1] + input_box[3];
+    output_box[3] = input_box[2];
+}
+
+void ex_thread::beq(){
+    //Compara si los registro A y B son iguales, pasa el PC branch a IF
+    if(input_box[1] == input_box[2]){
+        branch_result = true;
+    }else{
+        branch_result = false;
+    }
+
+    output_box[4] = input_box[0];
+}
+
+void ex_thread::bne(){
+    //Compara si los registro A y B son distintos, pasa el PC branch a IF
+    if(input_box[1] != input_box[2]){
+        branch_result = true;
+    }else{
+        branch_result = false;
+    }
+
+    output_box[0] = input_box[4];
+}
+
+void ex_thread::jal(){
+    //Pasa el PC branch a IF
+    output_box[0] = input_box[4];
+    branch_result = true;
+}
+
+void ex_thread::jalr(){
+    //Pasa el PC branch a IF
+    output_box[2] = input_box[1];
+    output_box[4] = input_box[0];
+    branch_result = true;
 }
--- qt_pipeline/ex_thread.h
+++ qt_pipeline/ex_thread.h
@@ -7,7 +7,7 @@
 class ex_thread
 {
 public:
-    bool branch_result=false;
+    bool branch_result = false;
     int  overwrite_cycles=0;
     Barrier* master_bar;
     Barrier* final_bar;
@@ -17,6 +17,25 @@ public:
 
     ex_thread();
     void run (void* data);
+
+    void executePhase(); //Ejecuta la etapa evaluando el IR.
+
+    void passInstrToMem(int instruction); //Pasa la instruccion del parametro al buzon IR de MEM.
+
+    void addi();
+    void add();
+    void sub();
+    void mul();
+    void div();
+    void lw();
+    void lr();
+    void sw();
+    void sc();
+    void beq();
+    void bne();
+    void jal();
+    void jalr();
+
 };
 
 #endif // EX_THREAD_H
